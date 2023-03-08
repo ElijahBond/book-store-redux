9c44c7c69a9f765b6ed6f5a0841242e0307eaf80
ElijahBond <bondarefil@gmail.com>
2023-03-08 12:13:08 +0300

    add components

---
+++ src/components/error-indicator/Error-indicator.js
@@ -0,0 +1,8 @@
+
+const ErrorIndicator = () => {
+    return (
+        <div>Error!</div>
+    )
+}
+
+export default ErrorIndicator;
---
+++ src/services/bookstore-service.js
@@ -0,0 +1,9 @@
+
+class BookstoreService {
+
+    getBooks() {
+        return [];
+    }
+};
+
+export default BookstoreService;
